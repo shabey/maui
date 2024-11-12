50c644a224623b4a5c25cf1ec76435ea19f8efaa
Jakub Florkowski <kubaflo123@gmail.com>
2024-11-12 20:12:23 +0100

    Removed unused usings

--- src/Core/src/Handlers/Border/BorderHandler.iOS.cs
+++ src/Core/src/Handlers/Border/BorderHandler.iOS.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Formats.Asn1;
-using System.Linq;
-using CoreAnimation;
-using Foundation;
-using Microsoft.Maui.Graphics;
 using UIKit;
 using PlatformView = UIKit.UIView;
 
