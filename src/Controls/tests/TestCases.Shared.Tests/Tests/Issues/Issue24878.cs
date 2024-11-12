8c80894858427a3f1aede111f15cd44b4c6895d3
Jakub Florkowski <kubaflo123@gmail.com>
2024-11-13 00:57:25 +0100

    Update Issue24878

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue24878.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue24878.cs
@@ -13,7 +13,6 @@ public class Issue24878 : _IssuesUITest
 
 	[Test]
 	[Category(UITestCategories.ToolbarItem)]
-	[FailsOnWindows("ToolbarItem changes text color on theme change, but support for icon color is not implemented")]
 	public void ToolbarItemsShouldBeVisible()
 	{
 		App.WaitForElement("label");
