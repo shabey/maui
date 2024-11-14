b0e7634d2e640937a20d2aa8cc5fdc5ef3d3b21e
Jakub Florkowski <kubaflo123@gmail.com>
2024-11-14 19:24:48 +0100

    Update Issue21488.cs

--- src/Controls/tests/TestCases.HostApp/Issues/Issue21488.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue21488.cs
@@ -8,11 +8,18 @@ public partial class Issue21488 : ContentPage
 	{
 		var buttonWithoutCharacterSpacing = new Button();
 		var buttonWithCharacterSpacing = new Button() { CharacterSpacing = 15 };
+		var buttonWithCharacterAndContentLayout = new Button()
+		{
+			CharacterSpacing = 15,
+			ContentLayout = new Button.ButtonContentLayout(Button.ButtonContentLayout.ImagePosition.Left, 10),
+			ImageSource = "dotnet_bot.png"
+		};
 		var entry = new Entry() { AutomationId = "Entry" };
 		entry.TextChanged += (s, e) =>
 		{
 			buttonWithoutCharacterSpacing.Text = entry.Text;
 			buttonWithCharacterSpacing.Text = entry.Text;
+			buttonWithCharacterAndContentLayout.Text = entry.Text;
 		};
 
 		Content = new VerticalStackLayout()
@@ -21,6 +28,8 @@ public partial class Issue21488 : ContentPage
 			buttonWithoutCharacterSpacing,
 			new Label() { Text = "Button with character spacing:" },
 			buttonWithCharacterSpacing,
+			new Label() { Text = "Button with character spacing and content layout:" },
+			buttonWithCharacterAndContentLayout,
 			entry
 		};
 	}
